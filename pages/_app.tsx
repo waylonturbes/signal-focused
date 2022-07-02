142864f7f2762335fcf060c6c4afa675c1132586
Waylon Turbes <83845399+waylonturbes@users.noreply.github.com>
2022-07-02 11:16:07 -0700

    feat: rename global css to main; nav link class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/main.css";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
