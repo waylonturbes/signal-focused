06b839402f0d13545ab911f8afe66d49632a8e57
Waylon Turbes <83845399+waylonturbes@users.noreply.github.com>
2022-07-09 15:39:46 -0700

    build: install @headless-ui/react

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,28 +2,28 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: "class",
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Inter", ...defaultTheme.fontFamily.sans],
-            },
-            backgroundImage: {
-                "signal-focused-background-light":
-                    "url('../public/signal-focused-background-light.png')",
-                "signal-focused-background-dark":
-                    "url('../public/signal-focused-background-dark.png')",
-            },
-        },
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        "signal-focused-background-light":
+          "url('../public/signal-focused-background-light.png')",
+        "signal-focused-background-dark":
+          "url('../public/signal-focused-background-dark.png')",
+      },
     },
-    variants: {
-        extend: {
-            backgroundImage: ["dark"],
-        },
+  },
+  variants: {
+    extend: {
+      backgroundImage: ["dark"],
     },
-    plugins: [],
+  },
+  plugins: [],
 };
