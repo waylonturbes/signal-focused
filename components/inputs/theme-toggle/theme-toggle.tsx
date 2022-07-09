f4950dc72635cade77279f42d606d706ec758c5a
Waylon Turbes <83845399+waylonturbes@users.noreply.github.com>
2022-07-09 15:39:54 -0700

    feat: turn theme toggle to btn; add icon-btn class

--- components/inputs/theme-toggle/theme-toggle.tsx
+++ components/inputs/theme-toggle/theme-toggle.tsx
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/outline";
 import { useTheme } from "next-themes";
 
-type ThemeToggleProps = {};
+interface ThemeToggleProps {
+  className?: string;
+}
+/**
+ * An unstyled button that can toggle the color mode.
+ * @param string - { className } A string used to style the button using Tailwind CSS classes
+ * @returns JSX.Element
+ */
+const ThemeToggle = ({ className }: ThemeToggleProps) => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-const ThemeToggle = ({}: ThemeToggleProps) => {
-    const [mounted, setMounted] = useState(false);
-    const { theme, setTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    useEffect(() => {
-        setMounted(true);
-    }, []);
+  if (!mounted) {
+    return null;
+  }
 
-    if (!mounted) {
-        return null;
-    }
-
-    return (
-        <select
-            value={theme}
-            onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                event.preventDefault();
-                setTheme(event.target.value);
-            }}
-        >
-            <option value="light">Light</option>
-            <option value="dark">Dark</option>
-        </select>
-    );
+  return (
+    <button
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className={className}
+    >
+      {theme === "light" ? (
+        <MoonIcon height="24px" width="24px" />
+      ) : (
+        <SunIcon height="24px" width="24px" />
+      )}
+    </button>
+  );
 };
 
 export { ThemeToggle };
