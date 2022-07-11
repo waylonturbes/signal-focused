87bd3d0d6df5411f35a771a23a6ed043cda9f60a
Waylon Turbes <83845399+waylonturbes@users.noreply.github.com>
2022-07-10 17:37:44 -0700

    feat: new classes; alter home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,28 @@
 import { ConsumerLayout } from "@components";
 import type { NextPageWithLayout } from "./_app";
 import type { ReactElement } from "react";
+import { ChevronRightIcon } from "@heroicons/react/outline";
+import Link from "next/link";
 
 const Home: NextPageWithLayout = () => {
   return (
     <>
-      <div className="text-center pt-32 pb-12 px-6 border-b space-y-6">
-        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-black">
+      <div className="text-center py-32 px-6 border-b">
+        <h1 className="text-6xl sm:text-7xl lg:text-8xl font-black mb-6">
           Seeing Through
           <br /> the <span className="text-chaos-gradient">Chaos</span>
         </h1>
-        <p className="text-xl">
+        <p className="text-xl sm:px-40 md:px-48 px-6 mb-10">
           Read articles on fantasy football, video games, cooking, and more.
         </p>
-        <button className="btn">Start Browsing</button>
+        <Link href="/articles" passHref>
+          <button className="btn btn-lg btn-primary btn-with-icon mx-auto px-6">
+            Start Browsing
+            <ChevronRightIcon className="left-btn-icon" />
+          </button>
+        </Link>
       </div>
+      <div className="text-center py-32 px-6 border-b bg-slate-50 dark:bg-slate-900"></div>
     </>
   );
 };
